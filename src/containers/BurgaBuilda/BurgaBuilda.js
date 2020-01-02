df368125f1961b337712a0571ea9a7a0d2cfdf1b
nazkin <nazar.kinash93@gmail.com>
2020-01-02 13:12:48 -0500

    The beginning of the burger app and the rough sketch of the component structure is starting to appear

---
+++ src/containers/BurgaBuilda/BurgaBuilda.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class BurgaBuilda extends Component {
+
+    render() {
+        return(
+           <Aux>
+              <div>Graphical Burger</div> 
+              <div>Ingredient Controller</div>
+           </Aux>
+        );
+    }
+}
+
+export default BurgaBuilda;
