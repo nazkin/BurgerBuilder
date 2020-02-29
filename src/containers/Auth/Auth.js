f8df50fa33a0ce6df6a4e76ef64f4aaf511a847a
nazkin <nazar.kinash93@gmail.com>
2020-02-29 13:22:57 -0500

    Theapp officially has a sign in feature working via JWT usign firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -13,74 +13,74 @@ class Auth extends Component {
  state = {
      controls: {
          email: {
-             elementtype: 'input',
+             inputtype: 'input',
              elementconfig: {
                  type: 'email',
                  placeholder: 'Enter Email'
              },
-             value: "",
-             validation: {
-                 required: true,
-                 isEmail: true
-             },
-             valid: false, 
-             touched: false
+             value: ""
+            //  validation: {
+            //      required: true,
+            //      isEmail: true
+            //  },
+            //  valid: false, 
+            //  touched: false
          },
          password: {
-            elementtype: 'input',
+            inputtype: 'input',
             elementconfig: {
                 type: 'password',
                 placeholder: 'Enter Password'
             },
-            value: "",
-            validation: {
-                required: true,
-                minLength: 6
-            },
-            valid: false, 
-            touched: false
+            value: ""
+            // validation: {
+            //     required: true,
+            //     minLength: 6
+            // },
+            // valid: false, 
+            // touched: false
         }
      }
  }
- checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-        return true;
-    }
+//  checkValidity(value, rules) {
+//     let isValid = true;
+//     if (!rules) {
+//         return true;
+//     }
     
-    if (rules.required) {
-        isValid = value.trim() !== '' && isValid;
-    }
+//     if (rules.required) {
+//         isValid = value.trim() !== '' && isValid;
+//     }
 
-    if (rules.minLength) {
-        isValid = value.length >= rules.minLength && isValid
-    }
+//     if (rules.minLength) {
+//         isValid = value.length >= rules.minLength && isValid
+//     }
 
-    if (rules.maxLength) {
-        isValid = value.length <= rules.maxLength && isValid
-    }
+//     if (rules.maxLength) {
+//         isValid = value.length <= rules.maxLength && isValid
+//     }
 
-    if (rules.isEmail) {
-        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        isValid = pattern.test(value) && isValid
-    }
+//     if (rules.isEmail) {
+//         const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+//         isValid = pattern.test(value) && isValid
+//     }
 
-    if (rules.isNumeric) {
-        const pattern = /^\d+$/;
-        isValid = pattern.test(value) && isValid
-    }
+//     if (rules.isNumeric) {
+//         const pattern = /^\d+$/;
+//         isValid = pattern.test(value) && isValid
+//     }
 
-    return isValid;
-}
+//     return isValid;
+// }
 
 inputChangedHandler = (event, controlName)=> {
     const updateControl = {
         ...this.state.controls,
         [controlName]: {
             ...this.state.controls[controlName],
-            value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
-            touched: true
+            value: event.target.value
+            // valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+            // touched: true
         }
     }
     this.setState({controls: updateControl});
@@ -88,7 +88,7 @@ inputChangedHandler = (event, controlName)=> {
 
 submitHandler  = (event)=> {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email, this.state.controls.password);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
 }
  render() {
     const formElementsArr = [];
@@ -102,12 +102,9 @@ submitHandler  = (event)=> {
     const form = formElementsArr.map(formElement => (
         <Input 
             key= {formElement.id}
-            elementtype = {formElement.config.elementtype}
+            inputtype = {formElement.config.inputtype}
             elementconfig = {formElement.config.elementconfig}
             value = {formElement.config.value}
-            invalid = {!formElement.config.valid}
-            shouldValidate = {formElement.config.validation}
-            touched = {formElement.config.touched}
             changed = {(event)=> this.inputChangedHandler(event, formElement.id)}
         />
     ))
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-
+import axios from 'axios';
 export const authStart = ()=> {
     return{
         type: actionTypes.AUTH_START
@@ -22,6 +22,19 @@ export const authFail = (err)=> {
 export const auth = (email, password)=> {
     return dispatch => {
         dispatch(authStart());
-        
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCN7tLvaqrTUtBGUt-UApD84SoyBOIqRTI', authData)
+            .then(response=> {
+                console.log(response);
+                dispatch(authSuccess(response));
+            })
+            .catch(err=> {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     }
 }
